08f2fbe57b0e0ffd94d918c93305b4b5b11f2504
Ahmedhafez12 <73128924+Ahmedhafez12@users.noreply.github.com>
2022-02-12 14:38:23 +0200

    adjust db

--- controllers/register.js
+++ controllers/register.js
@@ -5,28 +5,22 @@ const handleRegister = (req, res, db , bcrypt) => {
 	}
 	const hash = bcrypt.hashSync(password);
 
-	db('login')
-	.returning('*')
-	.insert({
-		hash:hash,
-		email:email
+		db.transaction(trx =>{
+		trx.insert({
+			hash:hash,
+			email:email
+		}).into('login').returning('email').then( loginEmail => {
+			return trx('users').returning('*').insert({
+				email:loginEmail[0],
+				name:name,
+				joined: new Date()
+			}).then(user => {
+				res.json(user[0]);
+			})
+		}).then(trx.commit).catch(trx.rollback)
+	}).catch( err => {
+		res.status(400).json("unable to register");
 	})
-	.then(data => {
-		res.json(data[0]);
-	})
-	.catch(err => res.status(400).json("cannot % register!"))	
-
-	 db('users')
-		.returning('*')
-		.insert({
-			email: email,
-			name: name, 
-			joined: new Date()
-		})
-		.then(user => {
-			res.json(user[0]);
-		})
-		.catch(err => res.status(400).json("cannot register!"))	
 		
 } 
 module.exports = {
