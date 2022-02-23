df2cc19a8e7f29206852d8d746573cb8c49901ae
Ahmedhafez12 <73128924+Ahmedhafez12@users.noreply.github.com>
2022-02-23 18:28:10 +0200

    make it better

--- controllers/register.js
+++ controllers/register.js
@@ -11,7 +11,7 @@ const handleRegister = (req, res, db , bcrypt) => {
 			email:email
 		}).into('login').returning('email').then( loginEmail => {
 			return trx('users').returning('*').insert({
-				email:loginEmail[0].email[,
+				email:loginEmail[0].email,
 				name:name,
 				joined: new Date()
 			}).then(user => {
